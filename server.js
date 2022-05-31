d1143656a1ae37a64d93c9611ddbff0c47cf624f
timiksha <timi@gmail.com>
2022-05-31 21:18:29 +0530

    done

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-
 const express = require('express');
 const ShortUrl = require('./models/shortUrl')
 const mongoose=require('mongoose');
 const app = express();
 
 mongoose.connect('mongodb://localhost/urlShortener',{
-           useNewUrlParser : true , useUnifiedTopology : true
-
+    useNewUrlParser : true , useUnifiedTopology : true
 })
 
-
 app.set('view engine','ejs');
+
 app.use(express.urlencoded({ extended : false}))
 
 app.get('/', async(req,res) => {
     const shortUrls = await ShortUrl.find()
 res.render('index',{ shortUrls: shortUrls})
 })
+
 app.post('/shortUrls', async(req,res) =>{
   await ShortUrl.create({ full : req.body.fullUrl})
   res.redirect('/')
 })
+
 app.get('/:shortUrl', async(req,res) => {
     const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl})
     if(shortUrl ==null) return res.sendStatus(404)
